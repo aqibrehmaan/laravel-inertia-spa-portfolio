e63b19851c6cea71a2e16370ba461965701445d1
Aqib <aqibr777@gmail.com>
2022-09-18 22:01:42 +0500

    2-Implement the Components to Display the List of Skills on the Dashboard

--- app/Http/Controllers/SkillController.php
+++ app/Http/Controllers/SkillController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use Inertia\Inertia;
+use App\Models\Skill;
 use Illuminate\Http\Request;
 
 class SkillController extends Controller
 {
-    //
+    public function index()
+    {
+        return Inertia::render('Skills/All', [
+            'skills' => Skill::all(),
+        ]);
+    }
 }
