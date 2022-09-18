ad142db4f15650c200e18ef7f1599b1b30017d7e
Aqib <aqibr777@gmail.com>
2022-09-18 22:18:41 +0500

    3-Show the Project List in the Admin Panel

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use Inertia\Inertia;
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    //
+    public function index()
+    {
+        return Inertia::render('Projects/All', [
+            'projects' => Project::all(),
+        ]);
+    }
 }
